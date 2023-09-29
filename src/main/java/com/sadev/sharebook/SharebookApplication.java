03475e73ac07a4eab5f832700f240b6313649f3b
saknouche <developpeur.web90@gmail.com>
2023-09-29 17:32:06 +0200

    First commit

---
+++ src/main/java/com/sadev/sharebook/SharebookApplication.java
@@ -0,0 +1,14 @@
+package com.sadev.sharebook;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharebookApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/sadev/sharebook/SharebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadev.sharebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
